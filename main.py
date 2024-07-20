c9b052c4cfa1fd432aa74abd71713eee9199112c
Brigoliboon <95647084+Brigoliboon@users.noreply.github.com>
2024-07-20 11:19:58 +0800

    Update main.py
    dj

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from models.scrape import search_query
 from models.scrape import file_detail
 import uvicorn
 import json
-import os
 app = FastAPI()
-if os.environ.get("VERCEL") == "1":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+@app.post("/")
+def read_root():
+    return {"Detail": "This API is part of my side project java application named World Library developed by Boon Jefferson Brigoli."}
 @app.get("/search")
 def read_item(q: str):
     search = search_query(q)
@@ -17,9 +17,6 @@ def read_item(q: str):
         return json.loads(results)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-@app.post("/")
-def read_root():
-    return {"Detail": "This API is part of my side project java application named World Library developed by Boon Jefferson Brigoli."}
 @app.get("/detail")
 def get_item_detail(file_link):
     return file_detail.get_file_detail()
