26799fa505c6b76ede04129614897595f1fcfac1
Brigoliboon <95647084+Brigoliboon@users.noreply.github.com>
2024-07-24 00:09:35 +0800

    biggest update in history

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-from typing import Union
+
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from models.scrape import search_query
+from models.scrape import Query
 from models.scrape import file_detail
 import uvicorn
 import json
 app = FastAPI()
-@app.post("/")
-def read_root():
+@app.get("/")
+async def read_root():
     return {"Detail": "This API is part of my side project java application named World Library developed by Boon Jefferson Brigoli."}
 @app.get("/search")
 def read_item(q: str):
-    search = search_query(q)
+    search = Query(q)
     try:
-        results = search.get_search()
+        results = search.get_result()
         return json.loads(results)
     except Exception as e:
+        print(results)
         raise HTTPException(status_code=500, detail=str(e))
-@app.get("/test")
-def test(q:str):
-    search = search_query(q)
-    result = search.test_website()
-    return result
 @app.get("/detail")
 def get_item_detail(file_link):
     return file_detail.get_file_detail()
--- models/scrape.py
+++ models/scrape.py
@@ -1,50 +1,77 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from pydantic import BaseModel
+from typing import Union
 import datetime
 import requests
 import time
 import json
 #scrape content from requested network
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:95.0) Gecko/20100101 Firefox/95.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate'
+}
+base_url = 'https://pdfdrive.com'
 
 #the search query object will scrape the queried results from the source
-class search_query:
-    url = 'https://pdfdrive.com'
+def request_network(baseurl, params:dict|None = None):
+    url = baseurl
+    status = 200|int
+    try:
+        response = requests.get(url,params=params)
+        status = response.status_code()
+        if status == 200:
+            return response.content
+        return status
+    except:
+        return status
+class File(BaseModel):
+    name: Union[str, None]
+    poster: Union[str, None]
+    description: Union[str, None]
+    link: Union[str, None]
+    info: Union[str, None]
+    download_link: Union[str, None]
+class Query:
     def __init__(self, q:str, pagecount='', pubyear='', searchin='',em='') -> None:
         self.q = q
         self.pagecount = pagecount
         self.pubyear = pubyear
         self.searchin = searchin
         self.em = em
-    def get_search(self) -> json:
-        search_url = f'{self.url}/search'
-        response = requests.get(search_url,params={'q':self.q})
+        
+    def get_result(self):
+        search_url = f'{base_url}/search'
+        response = requests.get(search_url,headers=headers, params={'q': self.q, 'pagecount':self.pagecount, 'pubyear':self.pubyear, 'searchin':self.searchin, 'em':self.em})
         soup = BeautifulSoup(response.content, 'html.parser')
-        table = soup.find('div',attrs={'class':'files-new'}).ul
+        files = self.extract_files(soup)
+        return self.pass_data_to_json(files)
+    @staticmethod
+    def extract_files(soup: BeautifulSoup) -> list[dict]:
         files = []
+        table = soup.find('div', attrs={'class': 'files-new'}).ul
         for item in table.findAll('li'):
-            details={}
             file_container_right = item.find('div', class_='file-right')
             file_container_left = item.find('div', class_='file-left')
-            details['name'] = file_container_left.img['title']
-            details['poster'] = file_container_left.img['src']
-            details['description'] =''
-            details['link'] = f"{self.url}{file_container_right.a['href']}"
-            details['info'] = self.clean_data_info([span.text for span in file_container_right.div.findAll('span')])
-            details['download_preview_link'] = self.extract_download_link(details['link'])
-            files.append(details)
-        return self.pass_data_to_json(files)
-    def test_website(self):
-        search_url = f'{self.url}/search'
-        response = requests.get(search_url,params={'q':self.q})
-        soup = BeautifulSoup(response.content, 'html.parser')
-        return soup
+            file_data = {
+                'name': file_container_left.a.img['title'],
+                'poster': file_container_left.a.img['src'],
+                'description': '',
+                'link': f"{base_url}{file_container_right.a['href']}",
+                'info': Query.clean_data_info([span.text for span in file_container_right.div.findAll('span')]),
+                'download_preview_link': Query._extract_download_link(file_container_right.a['href'])
+            }
+            files.append(file_data)
+        return files
     @staticmethod
-    def clean_data_info(data):
+    def clean_data_info(data:list):
         while '·' in data:
             data.remove('·')
         return data
     @staticmethod
-    def extract_download_link(link:str)-> None:
+    def _extract_download_link(link:str)-> None:
         index=link.rfind('e')
         download_link = f'{link[:index]}d{link[index+1:]}'
         return download_link
@@ -71,7 +98,7 @@ class file_detail():
         html = driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
         file = soup.find('div',attrs={'class':"btn-group"}).a['href']
-        return f'{search_query.url}{file}'
+        return f'{base_url}{file}'
     @staticmethod
     def process_file_detail(container)-> None:
         file = {}
@@ -81,3 +108,5 @@ class file_detail():
         file['description'] = container.find('div', class_="quotes").text
         file['download-link'] = container.find('a', 'download-button-link')['href']
         return file
+q = Query('nice')
+print(q.get_result())
